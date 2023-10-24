7df4f5bc2333144572571fb72cfb83f2e1f3563d
LinusCrugnola <linus.crugnola@epfl.ch>
2023-10-24 23:15:53 +0200

    add word_permutation code

--- csacademy/word_permutation/word_permutation.cpp
+++ csacademy/word_permutation/word_permutation.cpp
@@ -1,9 +1,23 @@
 #include <bits/stdc++.h>
+#include <string>
 
 using namespace std;
 
-int main(int argc, char ** argv)
+int main()
 {
-    std::cout << "Hello, world!\n" << std::endl;
+    size_t N;
+    cin >> N;
+    vector<pair<string, int>> words;
+    for (size_t i = 0; i < N; ++i) {
+        string word;
+        cin >> word;
+        words.push_back(make_pair(word, i));
+    }
+
+    sort(words.begin(), words.end());
+
+    for(auto& word : words) {
+        cout << word.second + 1 << " ";
+    }
     return 0;
 }
