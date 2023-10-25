742a8d3236eb7154fa2648cfbafc4c35d4d37d5a
LinusCrugnola <linus.crugnola@epfl.ch>
2023-10-25 10:43:16 +0200

    add lightbulbs

---
+++ csacademy/lightbulbs/lightbulbs.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    char str[51];
+    bool pos = true;
+    uint64_t p = 0, m = 0;
+
+    cin.getline(str, 51);
+
+    for(size_t i = 0; i<51; ++i) {
+        p = p << 1;
+        m = m << 1;
+        if(str[i] == '1') {
+            if(pos)
+                p += 1;
+            else
+                m += 1;
+            pos = !pos;
+        }
+    }
+    
+    cout << (pos ? ((p<<1) - (m<<1)) : ((p<<1) - (m<<1) -1)) << endl;
+
+    return 0;
+}
